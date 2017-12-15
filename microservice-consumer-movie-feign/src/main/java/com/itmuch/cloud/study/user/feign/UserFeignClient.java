64667a758e629a22275c88716cebfe7cb6691a37
hgx615 <hgx615@163.com>
2017-12-15 10:39:20 +0800

    提交 整个项目结构到dev 分支上 2017-12-14 17:50

--- microservice-consumer-movie-feign/src/main/java/com/itmuch/cloud/study/user/feign/UserFeignClient.java
+++ microservice-consumer-movie-feign/src/main/java/com/itmuch/cloud/study/user/feign/UserFeignClient.java
@@ -1,14 +1,15 @@
 package com.itmuch.cloud.study.user.feign;
 
+import com.itmuch.cloud.study.user.entity.User;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.itmuch.cloud.study.user.entity.User;
-
 @FeignClient(name = "microservice-provider-user")
 public interface UserFeignClient {
-  @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-  public User findById(@PathVariable("id") Long id);
+     /* 注：在@FeignClient 接口的方法中不能使用 @GetMapping 类似的注解 */
+//    @GetMapping(value = "/{id}")
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public User findById(@PathVariable("id") Long id);
 }
